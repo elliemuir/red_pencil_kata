0c7dcf69fc37764c3ad234598c63ee5247824aa6
Ellie Muir <calamitybird@gmail.com>
2015-07-23 08:42:20 -0400

    second scenario (price reduction does not qualify for red pencil due to recent price change) passing

--- main/features/red_pencil.feature
+++ main/features/red_pencil.feature
@@ -6,3 +6,11 @@ Feature: Automatic red pencil promotion status evaluation for items
     And my item is not currently marked as a red pencil promotion
     When I reduce the price of my item by 15%
     Then the item should now be tagged as a red pencil promotion
+
+
+  Scenario: Red pencil promotion not triggered when price is reduced between 5% and 30% but price has previously changed in the last 30 days
+    Given I have an item for sale on the website
+    And the price of my item was last changed 15 days ago
+    And my item is not currently marked as a red pencil promotion
+    When I reduce the price of my item by 15%
+    Then the item should not be tagged as a red pencil promotion
--- main/features/step_definitions/red_pencil.rb
+++ main/features/step_definitions/red_pencil.rb
@@ -13,8 +13,12 @@ When(/^I reduce the price of my item by (\d+)%$/) do |percent|
   $item.current_price = $item.reduce_price(percent)
 end
 
-Then(/^the item should now be tagged as a red pencil promotion$/) do
-  fail "Item was not flagged as red pencil promotion" unless $item.is_currently_red_pencil_promotion
+Then(/^the item should (now|not) be tagged as a red pencil promotion$/) do |now_or_not|
+  if now_or_not == "now"
+    fail "Item was not flagged as red pencil promotion" unless $item.is_currently_red_pencil_promotion
+  elsif now_or_not == "not"
+    fail "Item flagged as red pencil promotion" if $item.is_currently_red_pencil_promotion
+  end
 end
 
 Given(/^I have an item for sale on the website$/) do
